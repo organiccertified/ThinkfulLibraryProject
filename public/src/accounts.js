de7f12335b40055f786f3cd4d6e131e4625cdb21
Erick M <erick.michelr@gmail.com>
2022-07-10 08:11:34 -0500

    Revision changes v2

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,40 +1,29 @@
 function findAccountById(accounts, id) {
-
-  return accounts.reduce((acc,account)=> {
-      (account.id === id) ? acc = account : {}; //can use find() instead
-      return acc;    
-  },[])
-
-  }
-
-
-  function sortAccountsByLastName(accounts) {
-    return accounts.sort((account1, account2) => (account1.name.last > account2.name.last) ? 1 : -1 )
+  return accounts.reduce((acc, account) => {
+    (account.id === id) ? acc = account: {}; //can use find() instead
+    return acc;
+  }, [])
 }
 
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((account1, account2) => (account1.name.last > account2.name.last) ? 1 : -1)
+}
 
 function getTotalNumberOfBorrows(account, books) {
-  let count = 0; 
-  let result = books.reduce((acc, borrowedTimes)=> {
-      let borrowId = borrowedTimes.borrows;
+  let count = 0;
+  let result = books.reduce((acc, borrowedTimes) => {
+    let borrowId = borrowedTimes.borrows;
 
-      count =  borrowId.reduce((acc, borrowedTimes)=> {
-              borrowedTimes.id == account.id ? acc += 1 : acc+=0;
-              return acc;
-      },0);
-      acc+=count
+    count = borrowId.reduce((acc, borrowedTimes) => {
+      borrowedTimes.id == account.id ? acc += 1 : acc += 0;
       return acc;
+    }, 0);
+    acc += count
+    return acc;
 
   }, 0)
   return result;
 }
-
-//HELPER FUNCTION
-// function findAuthor(id) {
-//   const findAuthor = (id) =>{
-//   return authors.find((writer) => writer.id == id);
-// }};
-
 
 function getBooksPossessedByAccount(account, books, authors) {
   const borrowing = books.filter((book) => {
--- public/src/books.js
+++ public/src/books.js
@@ -2,68 +2,54 @@ function findAuthorById(authors, id) {
   return authors.find((author) => author.id == id)
 }
 
-
 function findBookById(books, id) {
   return books.find((book) => book.id == id)
 }
 
 function partitionBooksByBorrowedStatus(books) {
-  let total = [], stillBorrowedBooks = [], returnedBooks = []
+  let total = [],
+    stillBorrowedBooks = [],
+    returnedBooks = []
 
-
-  books.forEach(book => {
-    book.borrows.some((borrow)=> borrow.returned == false) 
-      ? stillBorrowedBooks.push(book) : returnedBooks.push(book)
+    books.forEach(book => {
+    book.borrows.some((borrow) => borrow.returned == false) ?
+      stillBorrowedBooks.push(book) : returnedBooks.push(book)
   });
 
   total.push(stillBorrowedBooks)
   total.push(returnedBooks)
-
-  return total; 
-
-
+  return total;
 }
-    
 
 
 function getBorrowersForBook(book, accounts) {
-  /*
-  - A book object.
-  - An array of all account objects.
-  
-  It should return an array of ten or fewer account objects that represents the accounts given 
-  by the IDs in the provided book's `borrows` array. However, each account object 
-  should include the `returned` entry from the corresponding transaction object in the `borrows` array.
-  
-  
-  */
   let borrowers = []
   let authorData = {}
   let result = []
-  
-  book.borrows.forEach((borrow)=>
-  {
-    authorData = accounts.find((author)=> author.id == borrow.id);
-    newObject = {...borrow, ...authorData};
+
+  book.borrows.forEach((borrow) => {
+    authorData = accounts.find((author) => author.id == borrow.id);
+    newObject = {
+      ...borrow,
+      ...authorData
+    };
     borrowers.push(newObject);
-  
-   })
-  
-  if(borrowers.length<=10){
-    for(let i =0; i < borrowers.length; i++){
+
+  })
+
+  if (borrowers.length <= 10) {
+    for (let i = 0; i < borrowers.length; i++) {
       result[i] = borrowers[i]
     }
-  }else{
-    for(let i =0; i <10; i++){
+  } else {
+    for (let i = 0; i < 10; i++) {
       result[i] = borrowers[i]
-    } 
+    }
   }
-  
-  return result 
-  
-  }
-  
-  
+  return result
+}
+
+
 
 module.exports = {
   findAuthorById,
